49fce5e9354815d0307ab1085e193cf9a1b041c2
Vineet Dwivedi <dvineet9@gmail.com>
2023-12-26 15:03:54 +0530

    redux introduced & login/Signup working fine

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,26 +1,79 @@
 import React, { useRef, useState } from 'react';
 import Header from './Header';
-import {checkvalidData} from '../utils/validate' 
+import {checkvalidData} from '../utils/validate' ;
+import {createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile} from "firebase/auth";
+import {auth} from "../utils/firebase";
+import {useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { addUser } from '../utils/userSlice';
 
 
 const Login = () => {
     const [isSignInForm, SetIsSignInForm] = useState(true);
     const [errorMessage, SetErrorMessage] = useState(null);
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
 
+    const name = useRef(null);
     const email  = useRef(null);
     const password = useRef(null);    
 
 
-    const toggleSignform =() =>{
-        SetIsSignInForm(!isSignInForm);
-    };
+   
 
     const handleButtonClick = () =>{
         // Validate the form data ...
         const message = checkvalidData(email.current.value, password.current.value);
         SetErrorMessage(message);
+
+        if(message) return;
+        //  Sign IN / Sign UP Logic
+        if (!isSignInForm){
+            createUserWithEmailAndPassword(auth, email.current.value, password.current.value)
+            .then((useCredential)=>{
+                const user = useCredential.user;
+                updateProfile(user, {
+            displayName: name.current.value, photoURL: "https://avatars.githubusercontent.com/u/60192525?v=4"
+            }).then(() => {
+                const {uid, email, displayName, photoURL} = auth.currentUser;
+                dispatch(addUser({uid: uid, email:email, displayName: displayName, photoURL:photoURL}));
+                navigate("/browse")
+            }).catch((error) => {
+                SetErrorMessage(error.message)
+            });
+                console.log(user);
+               
+            })
+            .catch((error)=>{
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                SetErrorMessage(errorCode+"-"+errorMessage)
+            });
+
+        }else{
+            signInWithEmailAndPassword(auth, email.current.value, password.current.value)
+            .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            console.log(user);
+            navigate("/browse")
+         })
+        .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        SetErrorMessage(errorCode+"-"+errorMessage)
+  });
+
+        }
     };
+
     
+    
+
+    const toggleSignform =() =>{
+        SetIsSignInForm(!isSignInForm);
+    };
+
 
   return (
     <div>
@@ -32,7 +85,7 @@ const Login = () => {
         </div>
         <form  onSubmit={(e)=>e.preventDefault()}className='w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white bg-opacity-80 rounded-lg'>
             <h1 className='font-bold text-3xl py-4'>{isSignInForm? "Sign In":"Sign Up"}</h1>
-            {!isSignInForm && <input type='text' placeholder='Full Name' className='p-4 my-4 w-full bg-gray-700 rounded-lg'/>}
+            {!isSignInForm && <input ref = {name} type='text' placeholder='Full Name' className='p-4 my-4 w-full bg-gray-700 rounded-lg'/>}
             <input ref = {email} type='text' placeholder='Email Address' className='p-4 my-4 w-full bg-gray-700 rounded-lg'/>
             <input ref = {password}type='password' placeholder='Password' className='p-4 my-4 w-full bg-gray-700 rounded-lg'/>
             <p className='text-red-600 text-lg'>{errorMessage}</p>
--- src/utils/validate.js
+++ src/utils/validate.js
@@ -2,7 +2,7 @@ export const checkvalidData = (email, password) => {
 
     const isEmailValid = /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/.test(email);
 
-    const isPasswordValid = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()])(?=\\S+$).{8, 20}$/.test(password);
+    const isPasswordValid =  /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
 
     if(!isEmailValid) return "Email is not Vaild"
     if(!isPasswordValid) return "Password is incorrect"
