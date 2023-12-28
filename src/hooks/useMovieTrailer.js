c4ccfecaf8bb729882b428492aefca18be696251
vineetdwivedi11 <dvineet9@gamil.com>
2023-12-28 23:10:19 +0530

    trailer Added

---
+++ src/hooks/useMovieTrailer.js
@@ -0,0 +1,30 @@
+import { useEffect } from "react";
+import { API_OPTIONS } from "../utils/constant";
+import { useDispatch } from "react-redux";
+import { addTrailerVideo } from "../utils/moviesSlice";
+
+const useMovieTrailer = (movieId) =>{
+    const dispatch = useDispatch();
+  // const [trailerId,setTrailerId] = useState(null);
+
+  const getMovieVideos = async () =>{
+    const data = await fetch("https://api.themoviedb.org/3/movie/" +
+    movieId+ 
+    "/videos?language=en-US",API_OPTIONS);
+    const json = await data.json();
+    // Filtering trailer from whole Movie API...
+    const filterData = json.results.filter(video => video.type === 'Trailer');
+    const trailer = filterData.length?filterData[0]:json.results[0];
+    console.log('trailer',trailer);
+    // setTrailerId(trailer.key)
+    dispatch(addTrailerVideo(trailer))
+  };
+
+  useEffect(()=>{
+    getMovieVideos();
+  },[]);
+
+}
+
+
+export default useMovieTrailer;
